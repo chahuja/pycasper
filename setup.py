bfee6a0acfa0d1e3ade1569d9677d17d39139725
Chaitanya Ahuja <ahujachaitanya@gmail.com>
2018-04-24 00:56:12 -0400

    updated setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from os import path
 
 setup(
   name='PyCasper',
-  version='0.1.1.dev2',
+  version='0.1.1.dev3',
   url='https://github.com/chahuja/pycasper',
   author='Chaitanya Ahuja',
   author_email='ahujachaitanya@gmail.com',
